655cdbbcf34a0a7521946cd0b1383ae2b7cd91c9
ozgeonec <ozgenuronec@gmail.com>
2020-12-17 17:14:02 +0300

    image settings continued

--- components/action-box/action-box.js
+++ components/action-box/action-box.js
@@ -1,13 +1,14 @@
 import styles from './action-box.module.css'
 import cn from 'classnames'
-import {Likesblue} from "../icons";
+import {Likesblue, Unlikedİcon2X} from "../icons";
 import TextSmall from "../text/text-small";
 
 
-function ActionBox({ className, ...props }) {
+function ActionBox({ className,count,blue=false, ...props }) {
     return (
         <div className={styles.box}{...props}>
-            <Likesblue/>
+            <TextSmall>{count}</TextSmall>
+            {blue ? <Likesblue/> : <Unlikedİcon2X/>}
             <TextSmall>Ty, Daniel, Homer and other 3 people</TextSmall>
         </div>
     )
--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -7,7 +7,7 @@ function Navigation({ children, className, ...props }) {
     return (
         <div className={styles.nav}{...props}>
            <h1 className={styles.logo}>pembepanjur</h1>
-            <Avatar/>
+            <Avatar size="30px"/>
         </div>
     )
 }
