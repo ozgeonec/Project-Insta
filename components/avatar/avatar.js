a7b685b06e8057c260d3f6a41989757ed984c7f5
ozgeonec <ozgenuronec@gmail.com>
2020-12-17 19:28:01 +0300

    small fixes

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import cn from 'classnames'
 import styles from './avatar.module.css'
 
-function Avatar({ src, alt, size = 25,border=false, ...props }) {
+function Avatar({ src, alt, size = 30,border=false, ...props }) {
     return (
         <button className={cn([styles.photo] , border && styles.border)} style={{ width: size, height: size }} {...props}>
             <img
